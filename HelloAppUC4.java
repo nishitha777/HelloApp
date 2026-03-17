2157db9fa9b63bd3d1b7bc6032ceee67b04d6751
nishitha777 <nk4565@srmist.edu.in>
2026-03-17 19:49:24 +0530

    Add files via upload

---
+++ HelloAppUC4.java
@@ -0,0 +1,26 @@
+/**
+ * @author nishi
+ * @version 1.0
+ */
+public class HelloApp {
+    public static void main(String[] args) {
+
+        String name = "World";
+
+        if (args.length > 0) {
+            StringBuilder names = new StringBuilder();
+
+            for (int i = 0; i < args.length; i++) {
+                names.append(args[i]);
+
+                if (i < args.length - 1) {
+                    names.append(", ");
+                }
+            }
+
+            name = names.toString();
+        }
+
+        System.out.println("Hello, " + name + "!");
+    }
+}
